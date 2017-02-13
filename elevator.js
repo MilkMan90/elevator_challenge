2de04985b5231ed34a038438bc381971b849a353
MilkMan90 <mlkaufman747@gmail.com>
2017-02-13 15:45:56 -0700

    tests passing

--- elevator.js
+++ elevator.js
@@ -1,8 +1,34 @@
 export default class Elevator {
   constructor() {
+    this.state = 'idle'; //'broken' or 'moving'
+    this.currentFloor = 0;
+    this.riders = [];
+    this.requests = [];
+    this.stops = 0;
+    this.floors = 0;
   }
 
   reset() {
     this.constructor()
   }
+
+  requestFloor(person, floor){
+    this.requests.push(Object.assign(person, {floor}))
+    //run requests
+    this.runElevator()
+    this.state = 'idle';
+  }
+
+  runElevator(){
+    this.requests.forEach((request)=>{
+      let pickupFloor = request.currentFloor;
+      let floorsToPerson = Math.abs(this.currentFloor - pickupFloor)
+      let floorsToDropoff = Math.abs(pickupFloor - request.floor)
+      this.currentFloor = request.floor;
+      this.floors = this.floors + floorsToPerson + floorsToDropoff;
+      this.stops += 2
+      this.requests.shift()
+    })
+  }
+
 }
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -14,7 +14,7 @@ describe('Elevator', function() {
     elevator.reset();
   });
 
-  xit('should bring a rider to a floor above their current floor', () => {
+  it('should bring a rider to a floor above their current floor', () => {
     // Alex requests the elevator to take him from 2 to 5
     elevator.requestFloor(alex, 5)
 
@@ -28,7 +28,110 @@ describe('Elevator', function() {
     assert.equal(elevator.floors, 5)
   });
 
-  xit('should bring a rider to a floor below their current floor', () => {
+  it('should bring a rider to a floor below their current floor', () => {
+    // Alex requests the elevator to take him from 2 to 5
+    elevator.requestFloor(alex, 1)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 1)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 2)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 3)
   });
+
+  const bob = new Person("Bob", 3)
+  const sue = new Person("Sue", 6)
+
+  it('should bring both riders to their desired floors - test 1', () => {
+    // bob requests the elevator to take him from 3 to 9
+    elevator.requestFloor(bob, 9)
+    elevator.requestFloor(sue, 2)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 2)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 4)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 16)
+  });
+
+  // const bob = new Person("Bob", 3)
+  // const sue = new Person("Sue", 6)
+  //
+  it('should bring both riders to their desired floors - Both Go Up', () => {
+    // bob requests the elevator to take him from 3 to 9
+    elevator.requestFloor(bob, 9)
+    elevator.requestFloor(sue, 9)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 9)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 4)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 15)
+  });
+
+  // const bob = new Person("Bob", 3)
+  // const sue = new Person("Sue", 6)
+  //
+  it('should bring both riders to their desired floors - Bob up sue down', () => {
+    // bob requests the elevator to take him from 3 to 9
+    elevator.requestFloor(bob, 10)
+    elevator.requestFloor(sue, 1)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 1)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 4)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 19)
+  });
+
+  // const bob = new Person("Bob", 3)
+  // const sue = new Person("Sue", 6)
+  //
+  it('should bring both riders to their desired floors - Bob down sue Up', () => {
+    // bob requests the elevator to take him from 3 to 9
+    elevator.requestFloor(bob, 1)
+    elevator.requestFloor(sue, 9)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 9)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 4)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 13)
+  });
+
+
+  // const bob = new Person("Bob", 3)
+  // const sue = new Person("Sue", 6)
+  //
+  it('should bring both riders to their desired floors - Bob down sue down', () => {
+    // bob requests the elevator to take him from 3 to 1
+    elevator.requestFloor(bob, 1)
+    elevator.requestFloor(sue, 5)
+
+    // Assert the current floor of the elevator is the drop off floor
+    assert.equal(elevator.currentFloor, 5)
+    // Assert the current status of the elevator is idle after drop off
+    assert.equal(elevator.state, 'idle')
+    // Assert the total number of stops is 2 after drop off
+    assert.equal(elevator.stops, 4)
+    // Assert the total number of floors traversed
+    assert.equal(elevator.floors, 11)
+  });
+
 
 });
